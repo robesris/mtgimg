7edc0867c62e7062dce9df090c7d2d965ede8c88
Rob Esris <resris@paperlesspost.com>
2015-07-31 14:24:19 -0400

    Return multiverseid

--- mtgimg.rb
+++ mtgimg.rb
@@ -4,12 +4,14 @@ require 'net/http'
 
 post '/' do
   text = params[:text].sub("mtgimg ", "")
-	url = URI.parse("http://gatherer.wizards.com/Pages/Search/Default.aspx?name=+[#{text}]")
+  url = URI.parse("http://gatherer.wizards.com/Pages/Search/Default.aspx?name=+[#{text}]")
   req = Net::HTTP::Get.new(url.to_s)
   res = Net::HTTP.start(url.host, url.port) {|http|
     http.request(req)
   }
-  {text: res.body}.to_json
+  match = res.body.match(/multiverseid%3d([0-9]*)/)
+  multiverseid = match[1]
+  {text: multiverseid}.to_json
 end
 
 get '/' do
@@ -19,5 +21,7 @@ get '/' do
   res = Net::HTTP.start(url.host, url.port) {|http|
     http.request(req)
   }
-  {text: res.body}.to_json
+  match = res.body.match(/multiverseid%3d([0-9]*)/)
+  multiverseid = match[1]
+  {text: multiverseid}.to_json
 end
