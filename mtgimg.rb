900b7a7cb074851a39e17e8bddd4aa1e3927493c
Rob Esris <resris@paperlesspost.com>
2015-07-31 14:50:57 -0400

    Removed get endpoint and log/comments.  Return blank string on failed search

--- mtgimg.rb
+++ mtgimg.rb
@@ -3,8 +3,6 @@ require 'json'
 require 'net/http'
 
 post '/' do
-  puts "PARAMS: #{params}"
-  puts "PARAMS[:TEXT]: #{params[:text]}"
   text = params[:text].sub("mtgimg ", "")
   text = text.split.inject(""){|str, word| "#{str}+[#{word}]"}
   url = URI.parse("http://gatherer.wizards.com/Pages/Search/Default.aspx?name=#{text}")
@@ -19,27 +17,6 @@ post '/' do
     image_url = "http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=#{multiverseid}&type=card"
     {text: image_url}.to_json
   else
-    res.body
-  end
-end
-
-get '/' do
-  puts "PARAMS: #{params}"
-  puts "PARAMS[:TEXT]: #{params[:text]}"
-  text = params[:text].sub("mtgimg ", "")
-  text = text.split.inject(""){|str, word| "#{str}+[#{word}]"}
-  url = URI.parse("http://gatherer.wizards.com/Pages/Search/Default.aspx?name=#{text}")
-  req = Net::HTTP::Get.new(url.to_s)
-  res = Net::HTTP.start(url.host, url.port) {|http|
-    http.request(req)
-  }
-  match = res.body.match(/multiverseid%3d([0-9]*)/)
-  match = res.body.match(/multiverseid[^0-9]*([0-9]*)/) unless match
-  if match
-    multiverseid = match[1]
-    image_url = "http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=#{multiverseid}&type=card"
-    {text: image_url}.to_json
-  else
-    res.body
+    ""
   end
 end
