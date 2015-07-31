766335696da7ff23a9b37c4ddb1996cc68ed938a
Rob Esris <resris@paperlesspost.com>
2015-07-31 14:39:29 -0400

    Tweak regex

--- mtgimg.rb
+++ mtgimg.rb
@@ -3,15 +3,22 @@ require 'json'
 require 'net/http'
 
 post '/' do
+  puts "PARAMS: #{params}"
+  puts "PARAMS[:TEXT]: #{params[:text]}"
   text = params[:text].sub("mtgimg ", "")
   url = URI.parse("http://gatherer.wizards.com/Pages/Search/Default.aspx?name=+[#{text}]")
   req = Net::HTTP::Get.new(url.to_s)
   res = Net::HTTP.start(url.host, url.port) {|http|
     http.request(req)
   }
-  match = res.body.match(/multiverseid%3d([0-9]*)/)
-  multiverseid = match[1]
-  {text: multiverseid}.to_json
+  match = res.body.match(/multiverseid[^0-9]*([0-9]*)/)
+  if match
+    multiverseid = match[1]
+    image_url = "http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=#{multiverseid}&type=card"
+    {text: image_url}.to_json
+  else
+    res.body
+  end
 end
 
 get '/' do
@@ -23,7 +30,7 @@ get '/' do
   res = Net::HTTP.start(url.host, url.port) {|http|
     http.request(req)
   }
-  match = res.body.match(/multiverseid%3d([0-9]*)/)
+  match = res.body.match(/multiverseid[^0-9]*([0-9]*)/)
   if match
     multiverseid = match[1]
     image_url = "http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=#{multiverseid}&type=card"
