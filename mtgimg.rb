d0bcb15c933bf98120f10b8e342ed6e72bca1ca8
Rob Esris <resris@paperlesspost.com>
2015-07-31 14:51:32 -0400

    Add error message

--- mtgimg.rb
+++ mtgimg.rb
@@ -17,6 +17,6 @@ post '/' do
     image_url = "http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=#{multiverseid}&type=card"
     {text: image_url}.to_json
   else
-    ""
+    {text: "Sorry, something went wrong.  It's probably your fault! D:"}
   end
 end
