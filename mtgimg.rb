f4de080ffbbc19209d9dd8ebfcbc6a0f093bff73
Rob Esris <resris@paperlesspost.com>
2015-07-31 14:12:55 -0400

    Try posting results from query

--- mtgimg.rb
+++ mtgimg.rb
@@ -3,6 +3,11 @@ require 'json'
 require 'uri'
 
 post '/' do
-	#url = URI.parse("http://gatherer.wizards.com/Pages/Search/Default.aspx?name=+[#{params[:text]}]")
-  #{text: params[:text]}.to_json
+  text = params[:text].sub("mtgimg ", "")
+	url = URI.parse("http://gatherer.wizards.com/Pages/Search/Default.aspx?name=+[#{text}]")
+  req = Net::HTTP::Get.new(url.to_s)
+  res = Net::HTTP.start(url.host, url.port) {|http|
+    http.request(req)
+  }
+  {text: res.body}.to_json
 end
