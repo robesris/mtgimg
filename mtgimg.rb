fd8dc478b670020e2c187e6c688a74a358f681d8
Rob Esris <resris@paperlesspost.com>
2015-07-31 14:17:06 -0400

    Get route for testing

--- mtgimg.rb
+++ mtgimg.rb
@@ -11,3 +11,13 @@ post '/' do
   }
   {text: res.body}.to_json
 end
+
+get '/' do
+  text = params[:text].sub("mtgimg ", "")
+  url = URI.parse("http://gatherer.wizards.com/Pages/Search/Default.aspx?name=+[#{text}]")
+  req = Net::HTTP::Get.new(url.to_s)
+  res = Net::HTTP.start(url.host, url.port) {|http|
+    http.request(req)
+  }
+  {text: res.body}.to_json
+end
