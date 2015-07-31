96ac54f5866325959545c760042f8e952840fe35
Rob Esris <resris@paperlesspost.com>
2015-07-31 14:04:26 -0400

    Return post text param

--- mtgimg.rb
+++ mtgimg.rb
@@ -2,5 +2,5 @@ require 'sinatra'
 require 'json'
 
 post '/' do
-  {text: "http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=87921&type=card"}.to_json
+  {text: params.POST.text}.to_json
 end
