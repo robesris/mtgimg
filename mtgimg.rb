08cdc96f469cf032e3e16fbfb41f4feda71a07c4
Rob Esris <resris@paperlesspost.com>
2015-07-31 20:26:02 -0400

    Add sarcastic error messages for searches that return no results

--- mtgimg.rb
+++ mtgimg.rb
@@ -3,6 +3,19 @@ require 'sinatra/multi_route'
 require 'json'
 require 'net/http'
 
+def no_match_message(search_text)
+  messages = [
+    "Nice try.",
+    "Seriously?",
+    "I don't think so.",
+    "Yes, there's a card named \"#{search_text}\".  No, really, there is.",
+    "I'm going to tell Rob you're trying to mess with his bot.",
+    "Nope.",
+    "There's really not that many of these sarcastic messages.  You've probably seen them all already.  Also, your search returned no results."
+  ]
+  messages.sample
+end
+
 route :get, :post, '/' do
   text = params[:text].sub("mtgimg ", "")
   text = text.split.inject(""){|str, word| "#{str}+[#{word}]"}
@@ -11,6 +24,8 @@ route :get, :post, '/' do
   res = Net::HTTP.start(url.host, url.port) {|http|
     http.request(req)
   }
+  return no_match_message(params[:text]) if res.body.match(/Your search returned zero results/)
+
   match = res.body.match(/multiverseid%3d([0-9]*)/)
   match = res.body.match(/multiverseid[^0-9]*([0-9]*)/) unless match
   if match
